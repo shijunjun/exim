65af176e6798a5c32ab84a25594da9f5959cda98
shijunjun <jun_5197@163.com>
2019-09-16 15:37:04 +0800

    del test

--- src/Export.php
+++ src/Export.php
@@ -5,14 +5,4 @@ class Export {
     public function test(){
         return [__FILE__,__DIR__];
     }
-    
-    /**
-     * 测试
-     * @author shijunjun
-     * @email jun_5197@163.com
-     * @date 2019年9月16日 下午3:35:33
-     */
-    public function __clone(){
-        exit;
-    }
 }
